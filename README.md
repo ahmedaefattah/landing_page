f55e4b9e4e248be90657ef1677c51bef6112eab7
ahmedaefattah <ahmed.aefattah@gmail.com>
2020-08-18 15:46:07 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,21 @@
+# Landing Page Project
+
+### created with HTML, CSS, Javascript
+
+## Project Description
+
+The website is a landing page, created as part of  Udacity Web Development Professional Nanodegree Program.
+
+This project requires you to build a multi-section landing page, with a dynamically updating navigational menu based on the amount of content that is added to the page.
+
+## Instructions
+
+The starter project has some HTML and CSS styling to display a static version of the Landing Page project. You'll need to convert this project from a static project to an interactive one. This will require modifying the HTML and CSS files, but primarily the JavaScript file.
+
+To get started, open `js/app.js` and start building out the app's functionality
+
+For specific, detailed instructions, look at the project instructions in the Udacity Classroom.
+
+
+
+
---
+++ js/app.js
@@ -0,0 +1,128 @@
+/**
+ * 
+ * Manipulating the DOM exercise.
+ * Exercise programmatically builds navigation,
+ * scrolls to anchors from navigation,
+ * and highlights section in viewport upon scrolling.
+ * 
+ * Dependencies: None
+ * 
+ * JS Version: ES2015/ES6
+ * 
+ * JS Standard: ESlint
+ * 
+*/
+
+/**
+ * Define Global Variables
+ * 
+*/
+const navbarList = document.getElementById('navbar__list');
+const landingContainer =  document.getElementsByClassName('landing__container').length;
+const sections = Array.from(document.querySelectorAll('section'));
+const scrollToUp = document.getElementById('scroll_to_up');
+const header = document.querySelector('.page__header');
+
+/**
+ * End Global Variables
+ * Begin Main Functions
+ * 
+*/
+
+// Build the navbar
+function addItemToNavbar() {
+  for(let i=1; i <= landingContainer; i++) {
+    let section = document.querySelector('#section' + i);
+    let sectionData = section.getAttribute("data-nav");
+    let sectionId = section.getAttribute("id");
+    let item    =  document.createElement("li");
+    let navLink  =  document.createElement("a");
+    navLink.textContent = sectionData;
+    navLink.setAttribute('class', 'menu__link');
+    navLink.setAttribute('href', '#' + sectionId );
+    item.appendChild(navLink);
+    navbarList.appendChild(item);
+  }
+}
+
+/* Add class 'active' to section when near top of viewport
+   and add class 'current'  to navbar item when the user scroll. */  
+function addActiveClass() {
+  sections.forEach(section => {
+    let bounding = section.getBoundingClientRect();
+    let sectionId = section.getAttribute("id");
+    let sectionLink = document.querySelector('a[href="#' + sectionId +  '"]');
+    let sectionHalfShown = section.offsetTop - (section.offsetHeight / 2);
+    let sectionBehind = section.offsetTop + (section.offsetHeight / 2);
+
+    if ((bounding.top >= 0) && (bounding.left >= 0) && (Math.floor(bounding.right) <= window.innerWidth) &&
+        (window.pageYOffset > sectionHalfShown) && (window.pageYOffset <= sectionBehind)) {
+          section.classList.add('active-section');
+          sectionLink.classList.add('active-link');
+    } else if (window.pageYOffset >= sectionBehind || window.pageYOffset < section.offsetTop) {
+          section.classList.remove('active-section');
+          sectionLink.classList.remove('active-link');
+    }
+  })
+}
+
+// Scroll to anchor ID using scrollTO event
+function scrollToSection(e) {
+  e.preventDefault();
+  let element =  document.querySelector(e.target.getAttribute('href'));
+  window.scrollTo(0, element.offsetTop);
+}
+
+// Scroll to up using scrollTO event
+function scrollToTop() { 
+    window.scrollTo(0, 0); 
+  } 
+
+//  Ensures navbar is visible on page load
+ function showNavbar() {
+  header.classList.add('show-navbar');
+};
+
+//  Hides navbar when user is not scrolling
+let scrolling = false;
+
+function hideNavbar(e) {
+  if (scrolling !== false) {
+    clearTimeout(scrolling);
+    header.classList.remove('hide-navbar');
+    header.classList.add('show-navbar');
+  }
+  scrolling = setTimeout(function () {
+    if (header.matches(':hover')) {
+      header.classList.remove('hide-navbar');
+    } else {
+      header.classList.remove('show-navbar');
+      header.classList.add('hide-navbar');
+    }
+  }, 1000);
+}
+
+/**
+ * End Main Functions
+ * Begin Events
+ * 
+*/
+
+// Build menu 
+addItemToNavbar();
+
+//  Ensures navbar is visible on page load
+window.addEventListener('onload', showNavbar );
+
+// Set sections as active
+window.addEventListener('scroll', addActiveClass);
+
+// Scroll to section on link click
+navbarList.addEventListener('click', scrollToSection);
+
+// Scroll to up on link click
+scrollToUp.addEventListener('click', scrollToTop);
+
+// Hides navbar when user is not scrolling
+window.addEventListener('scroll', hideNavbar, false);
+
